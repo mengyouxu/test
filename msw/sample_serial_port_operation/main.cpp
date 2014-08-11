ec883a626adc1087d296f8e5353dc7e28587c495
mengyouxu <mengyouxu@gmail.com>
2014-08-11 02:22:43 +0000

    1. 创建一个线程去监听COM4@115200，并将收到的data 用printf打印出来. 2. 主线程中用scanf()监听用户输入，若输入0，整个程序退出。 PS:    线程使用CreatThread()创建

--- msw/sample_serial_port_operation/main.cpp
+++ msw/sample_serial_port_operation/main.cpp
@@ -5,7 +5,42 @@
 
 #define DEBUG_PRINT_LINE() printf("%s()L%d\n",__func__,__LINE__)
 
+static int current_status;
 using namespace std;
+typedef struct{
+    HANDLE *hCom;
+}ARG_READ;
+
+DWORD WINAPI ThreadReadSerialPort(void *pM){
+    HANDLE *hCom;
+    //get serial port input
+    char lpInBuffer[1024];
+    DWORD dwBytesRead=1024;
+    BOOL bReadStatus;
+    DWORD dwErrorFlags;
+    COMSTAT ComStat;
+    OVERLAPPED m_osRead;
+    hCom = ((ARG_READ*)pM)->hCom;
+    int err = 0;
+    while(current_status == 1){
+        memset(lpInBuffer,'\0',1024);
+        //printf("[%d]Waiting for ...\n",__LINE__);
+        ClearCommError(*hCom,&dwErrorFlags,&ComStat);
+        dwBytesRead = 1024;
+        dwBytesRead = ReadFile(*hCom,lpInBuffer,dwBytesRead,&dwBytesRead,&m_osRead);
+        err = GetLastError();
+        if(bReadStatus == FALSE){
+            printf("LastError:%d\n",err);
+            if(err==ERROR_IO_PENDING){
+                GetOverlappedResult(*hCom,&m_osRead,&dwBytesRead,TRUE);
+                // GetOverlappedResult函数的最后一个参数设为TRUE，
+                //函数会一直等待，直到读操作完成或由于错误而返回。
+                printf("------> %s",lpInBuffer);
+                printf("Read %d\n",dwBytesRead);
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -13,12 +48,12 @@ int main()
 
     cout << "Hello world!" << endl;
 
-    hCom = CreateFile("COM5",
+    hCom = CreateFile("COM4",
 					GENERIC_READ|GENERIC_WRITE,
 					0, //独占方式
 					NULL,
 					OPEN_EXISTING, //打开而不是创建
-					0,//FILE_ATTRIBUTE_NORMAL|FILE_FLAG_OVERLAPPED, //异步
+					FILE_ATTRIBUTE_NORMAL|FILE_FLAG_OVERLAPPED, //异步
 					NULL);
 	if(hCom == INVALID_HANDLE_VALUE){
 		DEBUG_PRINT_LINE();
@@ -29,14 +64,14 @@ int main()
 
 	DCB dcb;
 	GetCommState(hCom,&dcb);
-    dcb.BaudRate=9600;
+    dcb.BaudRate=115200;
     dcb.ByteSize=8;
 	dcb.Parity=NOPARITY; //无奇偶校验位
 	dcb.StopBits=TWOSTOPBITS; //两个停止位
 	SetCommState(hCom,&dcb);
 
 	PurgeComm(hCom,PURGE_TXCLEAR|PURGE_RXCLEAR);//在读写串口之前，还要用PurgeComm()函数清空缓冲区
-
+/*  // 写数据到串口
 	char lpOutBuffer[100];
 	memset(lpOutBuffer,'0',100);
 	memcpy(lpOutBuffer,"hello world\n",12);
@@ -51,6 +86,20 @@ int main()
 	   	DEBUG_PRINT_LINE();
 	   	printf("WriteFile ERR:%d\n",GetLastError());
 	}
+*/
+    current_status=1;
+    ARG_READ arg;
+    arg.hCom=&hCom;
+    HANDLE handle = CreateThread(NULL, 0, ThreadReadSerialPort, &arg, 0, NULL);
+
+    while(1){
+        printf("[%d]Waiting for ...\n",__LINE__);
+        scanf("%d",&current_status);
+        if(current_status == 0) break;
+
+    }
+    WaitForSingleObject(handle, INFINITE);
+
 	PurgeComm(hCom, PURGE_TXABORT|PURGE_RXABORT|PURGE_TXCLEAR|PURGE_RXCLEAR);
 	/*
 		PURGE_TXABORT     中断所有写操作并立即返回，即使写操作还没有完成。
